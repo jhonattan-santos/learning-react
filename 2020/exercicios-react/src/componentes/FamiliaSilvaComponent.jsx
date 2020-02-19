a267ba2aaa06b55b76eaa8cd9da1cc7813e98cdd
Jhonattan Santos <jhonattanwesleysantos@gmail.com>
2020-02-19 18:45:28 -0300

    Relação entre componentes - Harded Code

---
+++ 2020/exercicios-react/src/componentes/FamiliaSilvaComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import MembroComponent from './MembroComponent' 
+
+export default props =>
+    <div>
+        <MembroComponent nome="Jhonattan" sobrenome="Silva"/>
+        <MembroComponent nome="Tati" sobrenome="Silva"/>
+        <MembroComponent nome="Pedro" sobrenome="Silva"/>
+        <MembroComponent nome="Maria" sobrenome="Silva"/>
+    </div>
--- 2020/exercicios-react/src/index.js
+++ 2020/exercicios-react/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import PrimeiroComponent from './componentes/PrimeiroComponente'
-import {CompA, CompB} from './componentes/DoisComponentes'
+// import {CompA, CompB} from './componentes/DoisComponentes'
+// import MultiElementosComponent from './componentes/MultiElementosComponent'
+import FamiliaSilvaComponent from './componentes/FamiliaSilvaComponent'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
     <div>
-        <CompA valor="Teste A!"></CompA>
-        <CompB valor="Teste B!"></CompB>
+        <FamiliaSilvaComponent />
+        {/* <MultiElementosComponent></MultiElementosComponent> */}
+        {/* <CompA valor="Teste A!"></CompA> */}
+        {/* <CompB valor="Teste B!"></CompB> */}
         {/* <PrimeiroComponent title="Primeiro componente passando title como props!"/> */}
     </div>
     , elemento)
